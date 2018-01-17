7d1049b4a9326358941c11b3ef29f29239471ba3
Justyna Bobrowska <justyna.bobrowska88@gmail.com>
2018-01-17 14:54:16 +0100

    compare arrays

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function add(a, b) {
-	return a + b
+var compareArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (var i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
-function multiply(c, d) {
-	return c * d
-}
-var multiplying = (multiply(36325, 9824));
-
-
-var all = add(multiplying, 777);
-console.log(all);
+console.log(compareArrays([1, 2, 3], [4, 5, "house"]));
+console.log(compareArrays([1, 2, 3], [1, 2, 3]));
+console.log(compareArrays([1, 2, 3], [1, 2, 3, 4]));
+console.log(compareArrays(["pig", "window", "opportunity"], ["pig", "window", "opporrtunity"]));
