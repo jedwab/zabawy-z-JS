f0962b18c9642c88c60127d2b57eda6910e996be
Justyna Bobrowska <justyna.bobrowska88@gmail.com>
2018-01-17 11:07:38 +0100

    awsome animals

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-var weekDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-var weatherAdjective = ["sunny", "windy", "snowy", "gloomy", "rainy", "cloudy", "foggy"];
-var temperature = ["20", "10", "-5", "0"];
+var animals = ["horse", "dog", "cat", "alpaca", "bear", "owl", "mouse"];
 
-var randomWeekDay = weekDay[Math.floor(Math.random() * weekDay.length)];
-var randomWeatherAdjective = weatherAdjective[Math.floor(Math.random() * weatherAdjective.length)];
-var randomTemperature = temperature[Math.floor(Math.random() * temperature.length)];
+for (i = 0 ; i < animals.length ; i ++) {
+	animals[i] = "Awsome " + animals[i];
+}
 
-var randomWeather = "On " + randomWeekDay + " the weather will be " + randomWeatherAdjective + " and the temperature will be " + randomTemperature +
-" Celsius degree. ";
-
-var comunicate = alert(randomWeather);
-comunicate();
+console.log(animals);
